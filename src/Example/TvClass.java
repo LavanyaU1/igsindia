cd0daaea14797ace6ed787fbf73225d636ae5b75
Sam <lavanya.u@igsindia.net>
2022-06-28 18:02:10 +0530

    changes added

--- src/Example/TvClass.java
+++ src/Example/TvClass.java
@@ -5,5 +5,8 @@ public class TvClass {
 	public static void main(String[] args) {
 		System.out.println("Hello TV");
 		System.out.println("Hello TV");
+		System.out.println("hw r u");
+		System.out.println("hi");
+		System.out.println("hi");
 	}
 }
--- src/Example/quality1.java
+++ src/Example/quality1.java
@@ -4,6 +4,7 @@ public class quality1 {
 
 	public static void main(String[] args) {
 	System.out.println("hello hw r u");
+	System.out.println("hello");
 	}
 
 }
