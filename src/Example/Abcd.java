0623defb995807e2961f50ac165c085296c09e95
Sam <lavanya.u@igsindia.net>
2022-06-28 12:35:42 +0530

    i added a abcd class

---
+++ src/Example/Abcd.java
@@ -0,0 +1,10 @@
+package Example;
+
+public class Abcd {
+
+	public static void main(String[] args) {
+		System.out.println("abcd class");
+
+	}
+
+}
--- src/Example/TvClass.java
+++ src/Example/TvClass.java
@@ -5,7 +5,7 @@ public class TvClass {
 	public static void main(String[] args) {
 		
 		System.out.println("Hello TV");
-
+		System.out.println("Hello TV");
 	}
 
 }
